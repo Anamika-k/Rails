d0fe36bc544579b934ca942cfd65ef5669781a02
Anamika Kaushik <anamikakaushik@Anamikas-MacBook-Air.local>
2023-04-05 15:53:15 +0530

    added changes in showing serializer

--- app/serializers/showing_serializer.rb
+++ app/serializers/showing_serializer.rb
@@ -1,5 +1,5 @@
 class ShowingSerializer < ActiveModel::Serializer
-  attributes :id, :Name, :Last_Name, :age
+  attributes :id, :Name, :Last_Name, :age, :name, :position
   def age
     #if object.DOB.present?
     #  Time.zone.now.year - object.DOB.year 
@@ -8,4 +8,11 @@ class ShowingSerializer < ActiveModel::Serializer
    #end
    object.DOB.present? ? (Time.zone.now.year - object.DOB.year) : ""
   end
+  def name
+    object.department.name
+  end
+  def position
+    object.DOJ.present? && (Time.zone.now.year - object.DOJ.year)>1 ? "Senior" : "junior"
+  end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'show', to:"employees#show"
   post 'save', to:"employees#save"
+  get 'index', to:"department#index"
+  post 'save_dept', to:"department#save_dept"
 end
