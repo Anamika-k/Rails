da913f6c5eb26579a5502b81ae71923e43f5c347
Anamika Kaushik <anamikakaushik@Anamikas-MacBook-Air.local>
2023-04-05 16:56:36 +0530

    Added a callback

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,9 @@
 class Employee < ApplicationRecord
     belongs_to :department
     validates :Name, uniqueness: true
+    after_create :setting_up_full_name
+
+    def setting_up_full_name
+        self.update(full_name: "#{self.Name} #{self.Last_Name}")
+    end
 end
